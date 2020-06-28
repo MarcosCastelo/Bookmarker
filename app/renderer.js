01f62013c625c905e64221e56bb5d61b38f6972c
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-06-27 23:52:22 -0300

    Adding the reference of html elements to renderer.js

---
+++ app/renderer.js
@@ -0,0 +1,6 @@
+const linksSection = document.querySelector('.links');
+const errorMessage = document.querySelector('.error-message');
+const newLinkForm = document.querySelector('new-link-form');
+const newLinkUrl = document.querySelector('.new-link-url')
+const newLinkSubmit = document.querySelector('.new-link-submit');
+const clearStorageButton = document.querySelector('.clear-storage');
