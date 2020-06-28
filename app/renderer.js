f9a789a3b2e7f804f21b3576ed26a8fb9ed29c82
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-06-28 00:08:29 -0300

    Refactoring the code

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,5 @@
+const parser = new DOMParser();
+
 const linksSection = document.querySelector('.links');
 const errorMessage = document.querySelector('.error-message');
 const newLinkForm = document.querySelector('new-link-form');
@@ -5,26 +7,28 @@ const newLinkUrl = document.querySelector('.new-link-url')
 const newLinkSubmit = document.querySelector('.new-link-submit');
 const clearStorageButton = document.querySelector('.clear-storage');
 
-newLinkUrl.addEventListener('keyup', () => {
-    newLinkSubmit.disabled = !newLinkUrl.validty.valid;
-});
-
 const clearForm = () => {
     newLinkForm.value = null;
 };
+
+const parseResponse = (text) => {
+    return parser.parseFromString(text, 'text/html');
+}
+
+const findTitle = (nodes) => {
+    return nodes.querySelector('title').innerText;
+}
 
 newLinkForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const url = newLinkUrl.value;
-    const parser = new DOMParser();
-    const parseResponse = (text) => {
-        return parser.parseFromString(text, 'text/html');
-    }
-
-    const findTitle = (nodes) => {
-        return nodes.querySelector('title').innerText;
-    }
     fetch(url)
-        .then(response => response.text());
+        .then(response => response.text())
+        .then(parseResponse)
+        .then(findTitle);
 })
+
+newLinkUrl.addEventListener('keyup', () => {
+    newLinkSubmit.disabled = !newLinkUrl.validty.valid;
+});
