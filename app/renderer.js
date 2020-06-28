95723ffea5f5d67b1defeff61c2ddcdd9d6191f9
MarcosCastelo <marcosvitorcastelo@gmail.com>
2020-06-27 23:57:09 -0300

    Creating the listerner to validate the url input field

--- app/renderer.js
+++ app/renderer.js
@@ -4,3 +4,11 @@ const newLinkForm = document.querySelector('new-link-form');
 const newLinkUrl = document.querySelector('.new-link-url')
 const newLinkSubmit = document.querySelector('.new-link-submit');
 const clearStorageButton = document.querySelector('.clear-storage');
+
+newLinkUrl.addEventListener('keyup', () => {
+    newLinkSubmit.disabled = !newLinkUrl.validty.valid;
+});
+
+const clearForm = () => {
+    newLinkForm.value = null;
+};
